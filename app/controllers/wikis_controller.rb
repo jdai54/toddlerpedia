0f4a40917615f25fdee7c2076e519683a5fc9ce1
Jake <jdai54@gmail.com>
2017-07-10 17:58:57 -0700

    CRUD completed

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,11 +4,56 @@ class WikisController < ApplicationController
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.private = params[:wiki][:private]
+
+    if @wiki.save
+      flash[:notice] = "Your wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error processing your wiki. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.private = params[:wiki][:private]
+
+    if @wiki.save
+      flash[:notice] = "The wiki was updated."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error processing the update. Please try again."
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash[:now] = "There was an error trying to delete your wiki. Please try again."
+      render :show
+    end
   end
 end
