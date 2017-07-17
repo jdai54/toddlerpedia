576605b6411eb6242b960bec2869d418b3a0e109
Jake <jdai54@gmail.com>
2017-07-17 11:53:18 -0700

    Added stripe, trying to associate stripe id and stripe subscription info with user

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,64 @@
+class ChargesController < ApplicationController
+  def new
+    @stripe_btn_data = {
+      key: "#{ Rails.configuration.stripe[:publishable_key]}",
+      description: "Premium Membership - #{current_user}",
+      amount: Amount.default
+    }
+  end
+
+  def create
+    # amount = Amount.default
+    plan = Stripe::Plan.create(
+      :name => "Premium Plan",
+      :id => "premium-monthly",
+      :interval => "month",
+      :currency => "usd",
+      :amount => Amount.default,
+    )
+    # Creates a Stripe Customer object, for associating with the ChargesController
+    customer = Stripe::Customer.create(
+      email: current_user,
+      card: params[:stripeToken]
+    )
+
+    subscription = Stripe::Subscription.create(
+      :customer => customer.id,
+      :plan => "premium-monthly",
+    )
+    current_user.update_attributes(stripe_id: customer.id)
+    current_user.update_attributes(stripe_subscription: subscription.id)
+    current_user.update_attributes(role: 'premium')
+    flash[:notice] = "Welcome to premium membership, #{current_user.email}! Start creating private wikis, and enjoy all the other benefits of premium membership today."
+    redirect_to wikis_path
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
+
+    def delete
+      customer = Stripe::Customer.retrieve(current_user.stripe_id)
+      subscription = Stripe::Subscription.retrieve(current_user.stripe_subscription)
+      subscription.delete
+      current_user.update_attributes(role: 'standard')
+      redirect_to edit_user_registration_path
+    end
+  #   # Where the real magic happens
+  #   charge = Stripe::Charge.create(
+  #     customer: customer.id, # Note -- this is NOT the user_id in your app
+  #     amount: Amount.default,
+  #     description: "BigMoney Membership - #{current_user.email}",
+  #     currency: 'usd'
+  #   )
+  #
+  #   flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
+  #   redirect_to user_path(current_user)
+  #
+  #   # Stripe will send back CardErrors, with friendly messages when something goes wrong.
+  #   # This 'rescue block' catches and displays those errors.
+  # rescue Stripe::CardError => e
+  #   flash[:alert] = e.message
+  #   redirect_to new_charge_path
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'charges/create'
+
   resources :wikis
+  resources :charges, only: [:new, :create]
   devise_for :users
-  
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
