498784955829a8f07415ef187f70690173f2704c
Jake <jdai54@gmail.com>
2017-07-25 18:21:46 -0700

    rake db:migrate not working- collaborators table already exists

---
+++ db/migrate/20170726011851_create_join_table_user_wiki.rb
@@ -0,0 +1,18 @@
+class CreateJoinTableUserWiki < ActiveRecord::Migration
+  def change
+    create_join_table :users, :wikis do |t|
+      t.index [:user_id, :wiki_id]
+      t.index [:wiki_id, :user_id]
+    end
+
+    create_table :collaborators do |t|
+      t.integer :user_id
+      t.integer :wiki_id
+      t.timestamps
+    end
+
+    add_index :collaborators, :id, unique: true
+    add_index :collaborators, :user_id
+    add_index :collaborators, :wiki_id
+  end
+end
